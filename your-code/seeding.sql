49764b7bc87de46eb2f1edb80c32e16bc566571c
naivm <miguel.vian@live.com.pt>
2019-08-20 18:20:51 +0100

    done

---
+++ your-code/seeding.sql
@@ -0,0 +1,13 @@
+
+insert into cars () values (0 , "Tiguan", "yellow", 2018, "3K096I98581DHSNUP" ), (1 , "Rifter", "red", 2019, "ZM8G7BEUQZ97IH46V" ),(2 , "Fusion", "white", 2019, "RKXVNNIHLVVZOUB4M" );
+
+insert into customers () values (0 , "Pablo Picasso", "+34636176382", "-" , "Madrid", "Madrid", "Spain", 28045 ), 
+(1 , "Abraham Lincoln", "+13059077086", "-", "Miami", "Florida", "USA", 33130 );
+
+insert into salespersons () values (0 , "Petey Cruiser", "Madrid"), 
+(1 , "Anna Sthesia", "Barcelona"),
+(2 , "Paul Molive", "Berlin");
+
+insert into invoices () values (0 , "2018-08-22", 1, 1, 2), 
+(1 , "2018-12-31", 0, 1, 0), 
+(2 , "2019-01-22", 2, 0, 2);
