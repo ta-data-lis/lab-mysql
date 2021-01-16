efbf1c9cecd9bb884febb8b8fd0e61818510df99
SRdG <sergioruizdegracia@gmail.com>
2021-01-16 19:04:58 +0100

    LAB | MySQL SRG
    LAB | MySQL SRG

---
+++ your-code/update SRG.sql
@@ -0,0 +1,19 @@
+USE lab_mysql;
+
+UPDATE Salesman
+SET Dealershop = 'Miami'
+WHERE Sales_ID = '00005'
+
+
+UPDATE Customers
+SET email = 'ppicasso@gmail.com'
+WHERE Customer_ID = '10001'
+
+
+UPDATE Customers
+SET email = 'lincoln@us.gov'
+WHERE Customer_ID = '20001'
+
+UPDATE Customers
+SET email = 'hello@napoleon.me'
+WHERE Customer_ID = '30001'
