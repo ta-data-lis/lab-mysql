237e547cfdefb1371c898fa04b3d1af61ed08ef8
Gladys Mawarni <gladysmawarni@gmail.com>
2021-10-31 18:08:51 +0000

    lab mysql

---
+++ your-code/update.sql
@@ -0,0 +1,17 @@
+/* update salesperson table */
+UPDATE lab_mysql.salespersons
+SET store = "Miami"
+WHERE id = 5;
+
+/* update customers table */
+UPDATE lab_mysql.customers
+SET email = "ppicasso@gmail.com"
+WHERE id = 1;
+
+UPDATE lab_mysql.customers
+SET email = "lincoln@us.gov"
+WHERE id = 2;
+
+UPDATE lab_mysql.customers
+SET email = "hello@napoleon.me"
+WHERE id = 3;
