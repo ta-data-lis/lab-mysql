e6da5d0b8b503a703de08f27a70846be3eb7fd2f
StanRocha <stanleysrocha@gmail.com>
2021-11-01 11:46:11 +0000

    done

---
+++ your-code/update.sql
@@ -0,0 +1,20 @@
+update car_company.customers
+set email = "ppicasso@gmail.com"
+where id_c = 0;
+
+update car_company.customers
+set email = "lincoln@us.gov"
+where id_c = 1;
+
+update car_company.customers
+set email = "hello@napoleon.me"
+where id_c = 2;
+
+
+select *
+from customers;
+
+
+
+
+
